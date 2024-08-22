0494ae2e5e99affda957d73f64e557aa154dd226
mousegames <stav.kowadlo@gmail.com>
2024-08-23 08:25:25 +1000

    pyText 1.0

--- pytext.py
+++ pytext.py
@@ -1,2 +1,12 @@
-for i in range(10):
-    print('pytext is the best')
+lines = []
+print('\033[2J')
+
+while True:
+    line = input('')
+    if line == 'EXIT':
+        break
+    lines.append(line)
+
+with open(input(''), 'w') as file:
+    for line in lines:
+        file.write(f'{line}\n')
